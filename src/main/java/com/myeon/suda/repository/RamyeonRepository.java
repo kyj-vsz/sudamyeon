474bd5a47b31af13d301a5f65dd72914b98c6902
유저이름 <asdfas@naver.com>
2023-10-26 13:33:54 +0900

    정키커밋

--- src/main/java/com/myeon/suda/repository/RamyeonRepository.java
+++ src/main/java/com/myeon/suda/repository/RamyeonRepository.java
@@ -9,10 +9,10 @@ import org.springframework.data.repository.query.Param;
 import com.myeon.suda.entity.Ramyeon;
 
 public interface RamyeonRepository extends JpaRepository<Ramyeon, Long>{
-    @Query(value = "SELECT r.*, i.inum, i.img_name, i.path, i.uuid, AVG(COALESCE(rv.grade, 0)), COUNT(rv.rno) " +
-            " FROM ramyeon r LEFT OUTER JOIN image i ON i.ramyeon_mno = r.mno " +
-            " LEFT OUTER JOIN review rv ON rv.ramyeon_mno = r.mno " +
-            " WHERE r.mno = ? GROUP BY i.inum ", nativeQuery = true)
+    @Query("SELECT r, i, AVG(COALESCE(rv.grade,0)),COUNT(rv.rno) " +
+    " FROM Ramyeon r LEFT OUTER JOIN Image i ON i.ramyeon = r " +
+    " LEFT OUTER JOIN Review rv ON rv.ramyeon = r " +
+    " WHERE r.mno = :mno GROUP BY i ")        
     List<Object[]> get_ramyeon_with_all(@Param("mno") Long mno);
     
     @Query("SELECT r, i, AVG(COALESCE(rv.grade,0)), COUNT(rv.rno) " +
